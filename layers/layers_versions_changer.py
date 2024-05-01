4da44e6d77e749dc37aa479f52bc5be5e7f61d1d
kreanimator <backin.v@yandex.ru>
2024-05-01 15:29:53 +0300

    Optimized code to change layer version only in Lambda templates for cloudformation and SAM

--- layers/layers_versions_changer.py
+++ layers/layers_versions_changer.py
@@ -1,22 +1,11 @@
 import os
 import boto3
-import yaml
-from yaml import Loader, SafeLoader
-
-
-def sub_constructor(loader, node):
-    value = loader.construct_scalar(node)
-    return {"Fn::Sub": value}
-
-
-Loader.add_constructor('!Sub', sub_constructor)
-SafeLoader.add_constructor('!Sub', sub_constructor)
-
 
 client = boto3.client('lambda')
-layer_change_key = 'SOSW_LAYER'
+layer_change_key = 'SOSW_LAYER_PLACEHOLDER'
 curr_runtime = 'python3.11'
 test_layer_name = "sosw"
+resource_types = ['AWS::Serverless::Function', 'AWS::Lambda::Function']
 
 
 def main():
@@ -24,7 +13,7 @@ def main():
         response = get_lambda_layer_version(curr_runtime, test_layer_name)
         print(response)
         if response:
-            change_lambda_layer_version(response, layer_change_key)
+            change_lambda_layer_version(response, layer_change_key,resource_types)
     except RuntimeError as r:
         raise RuntimeError(f"Something went wrong: {r}")
 
@@ -34,19 +23,27 @@ def get_lambda_layer_version(runtime: str, layer_name: str) -> dict:
     return response
 
 
-def change_lambda_layer_version(response: dict, change_key: str):
+def change_lambda_layer_version(response: dict, change_key: str, resource_types_to_change: list[str]):
     layer_version = response['LayerVersions'][0]['Version']
     print(f"Layer version: {layer_version}")
+    print(change_key)
     for root, dirs, files in os.walk('.'):
         for file in files:
             if file.endswith('.yaml'):
                 file_path = os.path.join(root, file)
                 with open(file_path, 'r') as f:
-                    yaml_data = yaml.safe_load(f)
-                    if change_key in yaml_data:
-                        yaml_data[change_key] = layer_version
-                        with open(file_path, 'w') as fw:
-                            yaml.dump(yaml_data, fw)
+                    if contains_resource_type(f, resource_types_to_change):
+                        lines = f.readlines()
+                        lines = [line.replace(change_key, str(layer_version)) for line in lines]
+                        with open(file_path, "w") as fw:
+                            fw.writelines(lines)
+
+
+def contains_resource_type(file_handle, resource_types_to_check: list[str]) -> bool:
+    for line in file_handle:
+        if 'Type' in line and any(resource_type in line for resource_type in resource_types_to_check):
+            return True
+    return False
 
 
 if __name__ == "__main__":
