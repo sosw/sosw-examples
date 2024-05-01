ce8874e9dda630436391e68d1dbb7479134b524e
kreanimator <backin.v@yandex.ru>
2024-05-01 16:36:58 +0300

    Changed functionality to check the folder where it is implemented from

--- layers/layers_versions_changer.py
+++ layers/layers_versions_changer.py
@@ -29,19 +29,21 @@ def change_lambda_layer_version(response: dict, change_key: str, resources_to_ch
     layer_version = response['LayerVersions'][0]['Version']
     print(f"Layer version: {layer_version}")
     print(f"Change key: {change_key}")
-    for root, dirs, files in os.walk('.'):
-        for file in files:
-            if file.endswith('.yaml'):
-                file_path = os.path.join(root, file)
+    # for root, dirs, files in os.walk('.'): # Use it only if you need scan files in all repository
+    for file in os.listdir("."):
+        if file.endswith('.yaml'):
+            # file_path = os.path.join(root, file) # Use it only if you need scan files in all repository
+            # for file in files:
+            file_path = os.path.join('.', file)
 
-                with open(file_path, 'r') as f:
-                    data = f.read()
-                    if not contains_resource_type(data, resources_to_change):
-                        print("Resource to change not found")
-                        continue
+            with open(file_path, 'r') as f:
+                data = f.read()
+                if not contains_resource_type(data, resources_to_change):
+                    print("Resource to change not found")
+                    continue
 
-                with open(file_path, "w") as f:
-                    f.write(data.replace(change_key, str(layer_version)))
+            with open(file_path, "w") as f:
+                f.write(data.replace(change_key, str(layer_version)))
 
 
 def contains_resource_type(data: str, resource_types_to_check: list[str]) -> bool:
